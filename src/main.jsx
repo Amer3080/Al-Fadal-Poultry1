f6e79c078778bcc21d3be0e9217c1cbd47430274
Amer3080 <mohamed.amer3080@gmail.com>
2025-09-17 00:33:08 +0300

    tesy

--- src/main.jsx
+++ src/main.jsx
@@ -15,9 +15,38 @@ import ReactDOM from "react-dom/client";
 import "wicg-inert";
 
 // Fonts Packages
+import "@fontsource/marhey/400.css";
+import "@fontsource/marhey/700.css";
+import "@fontsource/oleo-script/400.css";
+import "@fontsource/caveat/400.css";
+import "@fontsource/noto-nastaliq-urdu/400.css";
+import "@fontsource/el-messiri/400.css";
+import "@fontsource/fugaz-one/400.css";
+import "@fontsource/rubik/400.css";
+import "@fontsource/merienda/400.css";
+import "@fontsource/orbitron/400.css";
+import "@fontsource/archivo/400.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/700.css";
 
 const theme = createTheme({
   direction: "ltr",
+  typography: {
+    fontFamily: [
+      "Roboto",
+      "Marhey",
+      "Oleo Script",
+      "Caveat",
+      "Noto Nastaliq Urdu",
+      "El Messiri",
+      "Fugaz One",
+      "Rubik",
+      "Merienda",
+      "Orbitron",
+      "Archivo",
+      "sans-serif",
+    ].join(","),
+  },
 });
 
 createRoot(document.getElementById("root")).render(
